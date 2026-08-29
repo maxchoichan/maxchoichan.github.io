e48a11fd4f70e0dc0351a52fcf7e8ddac46e2007
maxchoichan <maxchoichan@users.noreply.github.com>
2026-08-29 11:32:40 +0800

    Add personal bio

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Max Wen",
-	bio: "",
+	bio: "Former image algorithm engineer, now a network engineer.",
 	links: [
 		{
 			name: "GitHub",
