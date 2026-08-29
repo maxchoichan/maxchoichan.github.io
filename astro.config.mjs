e153bdcbced247ebff147d51e4b25ead1d5af7d3
maxchoichan <maxchoichan@users.noreply.github.com>
2026-08-29 11:21:57 +0800

    Change site URL to maxchoichan.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://maxwen.github.io/",
+	site: "https://maxchoichan.github.io/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
