60637223be358748d34892710a7028709ec96498
WCZ93762 <WCZ93762@users.noreply.github.com>
2026-08-29 11:03:43 +0800

    Change site URL to maxwenblog.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://WCZ93762.github.io/",
+	site: "https://maxwenblog.github.io/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
